d78a76381861f994de1686bbe6fd95603545ceff
SashaGolovach <gsancho0001@gmail.com>
2020-12-11 23:59:23 +0200

    HomePage modified

--- build-server-front/src/HomePage/HomePage.tsx
+++ build-server-front/src/HomePage/HomePage.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 import 'antd/dist/antd.css';
 import { Form, Input, Button, Typography } from 'antd';
-
+import './HomePage.less'
 import { Layout, Menu, Breadcrumb } from 'antd';
 import {
-  DesktopOutlined,
-  PieChartOutlined,
+  SettingOutlined,
+  ToolOutlined,
+  CodeOutlined,
   FileOutlined,
-  TeamOutlined,
-  UserOutlined,
+  InfoCircleOutlined,
+  BarChartOutlined,
+  ProjectOutlined
 } from '@ant-design/icons';
 
 const { Title } = Typography;
@@ -16,49 +18,35 @@ const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
 const HomePage = () => {
-  const [collapsed, setCollapsed] = useState(true);
-  const onCollapse = (collapsed : any) => {
-    setCollapsed(collapsed);
+  //localStorage.removeItem('accessToken');
+  const [selectedItem, setSelectedItem] = useState('mail');
+
+  const handleClick = (e : any) => {
+    console.log('click ', e);
+    setSelectedItem(e.key);
   };
-  return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
-        <div className="logo" />
-        <Menu theme="light" defaultSelectedKeys={['1']} mode="inline">
-          <Menu.Item key="1" icon={<PieChartOutlined />}>
-            Option 1
-          </Menu.Item>
-          <Menu.Item key="2" icon={<DesktopOutlined />}>
-            Option 2
-          </Menu.Item>
-          <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-            <Menu.Item key="3">Tom</Menu.Item>
-            <Menu.Item key="4">Bill</Menu.Item>
-            <Menu.Item key="5">Alex</Menu.Item>
-          </SubMenu>
-          <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-            <Menu.Item key="6">Team 1</Menu.Item>
-            <Menu.Item key="8">Team 2</Menu.Item>
-          </SubMenu>
-          <Menu.Item key="9" icon={<FileOutlined />}>
-            Files
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className="site-layout">
-        <Header className="site-layout-background" style={{ padding: 0 }} />
-        <Content style={{ margin: '0 16px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>User</Breadcrumb.Item>
-            <Breadcrumb.Item>Bill</Breadcrumb.Item>
-          </Breadcrumb>
-          <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-            Bill is a cat.
-          </div>
-        </Content>
-      </Layout>
-    </Layout>
-  );
+
+  return <>
+      <Menu onClick={handleClick} selectedKeys={[selectedItem]} mode="horizontal">
+        <Menu.Item key="mail" icon={<ProjectOutlined />}>
+          Projects
+        </Menu.Item>
+        <Menu.Item key="app" disabled icon={<SettingOutlined />}>
+          Settings
+        </Menu.Item>
+        <SubMenu key="SubMenu" icon={<ToolOutlined />} title="Tools">
+          <Menu.ItemGroup title="Common">
+            <Menu.Item key="setting:1" icon={<CodeOutlined />}>Shell</Menu.Item>
+            <Menu.Item key="setting:2" icon={<FileOutlined />}>Files</Menu.Item>
+          </Menu.ItemGroup>
+          <Menu.ItemGroup title="Server">
+            <Menu.Item key="setting:3" icon={<BarChartOutlined />}>Metrics</Menu.Item>
+            <Menu.Item key="setting:4" icon={<InfoCircleOutlined />}>Logs</Menu.Item>
+          </Menu.ItemGroup>
+        </SubMenu>
+      </Menu>
+      <p>Projects</p>
+    </>
 };
 
 export default HomePage;
